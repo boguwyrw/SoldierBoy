0c21c3949109b000869dad3db44ccb813d289e3f
boguwyrw <b.wyrwal@wp.pl>
2020-12-01 16:16:13 +0100

    Player movement was finished

---
+++ SoldierBoy/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float playerSpeed = 4.0f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float movementVertical = Input.GetAxis("Vertical");
+        movementVertical *= Time.deltaTime;
+        transform.Translate(Vector3.forward * movementVertical * playerSpeed);
+
+        float movementHorizontal = Input.GetAxis("Horizontal");
+        movementHorizontal *= Time.deltaTime;
+        transform.Translate(new Vector3(movementHorizontal * playerSpeed, 0.0f, 0.0f));
+    }
+}
