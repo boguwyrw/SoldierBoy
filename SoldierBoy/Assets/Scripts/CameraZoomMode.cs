bd62953ba8ee9b0c97c32224faf54be1c7913a1f
boguwyrw <b.wyrwal@wp.pl>
2020-12-03 23:42:25 +0100

    Small Gun zooming has been finished

---
+++ SoldierBoy/Assets/Scripts/CameraZoomMode.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomMode : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cameraObject;
+
+    private Animator smallGunZoomAnim;
+    private Camera playerCamera;
+    private bool cameraZoom = false;
+    private float zoomSpeed = 100.0f;
+    private float maxZoom = 30.0f;
+    private float minZoom = 60.0f;
+
+    private void Start()
+    {
+        smallGunZoomAnim = GetComponent<Animator>();
+        playerCamera = cameraObject.GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            //smallGunZoomAnim.Play("SmallGunZoomAnimation");
+            smallGunZoomAnim.SetBool("isZoomOut", false);
+            smallGunZoomAnim.SetBool("isZoomIn", true);
+        }
+
+        if (Input.GetMouseButton(1) && playerCamera.fieldOfView >= maxZoom)
+        {
+            playerCamera.fieldOfView -= zoomSpeed * Time.deltaTime;
+        }
+
+        if (playerCamera.fieldOfView <= maxZoom)
+        {
+            cameraZoom = true;
+            //smallGunZoomAnim.SetBool("isZoomIn", false);
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            smallGunZoomAnim.SetBool("isZoomIn", false);
+            smallGunZoomAnim.SetBool("isZoomOut", true);
+        }
+
+        if (cameraZoom && playerCamera.fieldOfView <= minZoom)
+        {
+            playerCamera.fieldOfView += zoomSpeed * Time.deltaTime;
+        }
+
+        if (playerCamera.fieldOfView >= minZoom)
+        {
+            cameraZoom = false;
+        }
+    }
+}
